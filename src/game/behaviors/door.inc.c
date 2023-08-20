32ce9f2aee0b5fa176cf8724d8e72699339af40c
MarioGuyWasTakenAgain <135893416+MarioGuyWasTakenAgain@users.noreply.github.com>
2023-08-20 12:26:04 -0400

    Update door.inc.c

--- src/game/behaviors/door.inc.c
+++ src/game/behaviors/door.inc.c
@@ -15,15 +15,17 @@ s32 D_8032F330[] = { SOUND_GENERAL_CLOSE_WOOD_DOOR, SOUND_GENERAL_CLOSE_IRON_DOO
 
 void door_animation_and_reset(s32 sp18) {
     cur_obj_init_animation_with_sound(sp18);
-    if (cur_obj_check_if_near_animation_end())
+    if (cur_obj_check_if_near_animation_end()) {
         o->oAction = 0;
+    }
 }
 
 void set_door_camera_event(void) {
-    if (segmented_to_virtual(bhvDoor) == o->behavior)
+    if (segmented_to_virtual(bhvDoor) == o->behavior) {
         gPlayerCameraState->cameraEvent = CAM_EVENT_DOOR;
-    else
+    } else {
         gPlayerCameraState->cameraEvent = CAM_EVENT_DOOR_WARP;
+    }
     gPlayerCameraState->usedObj = o;
 }
 
@@ -34,10 +36,11 @@ void play_door_open_noise(void) {
         gTimeStopState |= TIME_STOP_MARIO_OPENED_DOOR;
     }
     if (o->oTimer == 50) {
-        if (o->oBehParams2ndByte == 1)
+        if (o->oBehParams2ndByte == 1) {
             play_secondary_music(0x23, 0x4B, 0xFF, 0);
-        else if (o->oBehParams2ndByte == 2)
+        } else if (o->oBehParams2ndByte == 2) {
             play_secondary_music(0x24, 0x4B, 0xFF, 0);
+        }
     }
     if (o->oTimer == 70) {
         cur_obj_play_sound_2(D_8032F330[sp1C]);
@@ -47,8 +50,9 @@ void play_door_open_noise(void) {
 
 void play_warp_door_open_noise(void) {
     s32 sp1C = cur_obj_has_model(MODEL_HMC_METAL_DOOR);
-    if (o->oTimer == 30)
+    if (o->oTimer == 30) {
         cur_obj_play_sound_2(D_8032F330[sp1C]);
+    }
 }
 
 void bhv_door_loop(void) {
@@ -83,8 +87,9 @@ void bhv_door_loop(void) {
             play_warp_door_open_noise();
             break;
     }
-    if (o->oAction == 0)
+    if (o->oAction == 0) {
         load_object_collision_model();
+    }
     bhv_star_door_loop_2();
 }
 
@@ -119,29 +124,27 @@ void bhv_door_init(void) {
 
 void bhv_star_door_loop_2(void) {
     s32 sp4 = 0;
+
     if (gMarioCurrentRoom != 0) {
-        if (o->oDoorUnkF8 == gMarioCurrentRoom)
+        if (o->oDoorUnkF8 == gMarioCurrentRoom ||
+            gMarioCurrentRoom == o->oDoorUnkFC ||
+            gMarioCurrentRoom == o->oDoorUnk100 ||
+            gDoorAdjacentRooms[gMarioCurrentRoom][0] == o->oDoorUnkFC ||
+            gDoorAdjacentRooms[gMarioCurrentRoom][0] == o->oDoorUnk100 ||
+            gDoorAdjacentRooms[gMarioCurrentRoom][1] == o->oDoorUnkFC ||
+            gDoorAdjacentRooms[gMarioCurrentRoom][1] == o->oDoorUnk100) {
             sp4 = 1;
-        else if (gMarioCurrentRoom == o->oDoorUnkFC)
-            sp4 = 1;
-        else if (gMarioCurrentRoom == o->oDoorUnk100)
-            sp4 = 1;
-        else if (gDoorAdjacentRooms[gMarioCurrentRoom][0] == o->oDoorUnkFC)
-            sp4 = 1;
-        else if (gDoorAdjacentRooms[gMarioCurrentRoom][0] == o->oDoorUnk100)
-            sp4 = 1;
-        else if (gDoorAdjacentRooms[gMarioCurrentRoom][1] == o->oDoorUnkFC)
-            sp4 = 1;
-        else if (gDoorAdjacentRooms[gMarioCurrentRoom][1] == o->oDoorUnk100)
-            sp4 = 1;
-    } else
+        }
+    } else {
         sp4 = 1;
+    }
+
     if (sp4 == 1) {
         o->header.gfx.node.flags |= GRAPH_RENDER_ACTIVE;
         D_8035FEE4++;
-    }
-    if (sp4 == 0) {
+    } else {
         o->header.gfx.node.flags &= ~GRAPH_RENDER_ACTIVE;
     }
+
     o->oDoorUnk88 = sp4;
 }
